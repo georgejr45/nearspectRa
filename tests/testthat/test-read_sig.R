dd0e616f14c376a22adf833b02a6754c72b0d439
georgejr <methungeorge333@gmail.com>
2024-07-18 11:24:39 +0200

    error solved in unittest

--- tests/testthat/test-read_sig.R
+++ tests/testthat/test-read_sig.R
@@ -27,9 +27,9 @@ test_that("read.sig function works correctly", {
   expect_equal(colnames(data), c("Wavelengths(nm)", "Reference Values", "Target Values", "Reflectance(%)"))
 
   # Check the content of the data frame
-  expect_equal(data[1, ], c(400, 0.1, 0.2, 50))
-  expect_equal(data[2, ], c(500, 0.3, 0.4, 60))
-  expect_equal(data[3, ], c(600, 0.5, 0.6, 70))
+  expect_equal(as.numeric(data[1,]), c(400, 0.1, 0.2, 50))
+  expect_equal(as.numeric(data[2, ]), c(500, 0.3, 0.4, 60))
+  expect_equal(as.numeric(data[3, ]), c(600, 0.5, 0.6, 70))
 
   # Clean up the temporary file
   unlink(temp_file)
