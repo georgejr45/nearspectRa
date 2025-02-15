9194dbf5620e72bca7dd55a1a5161340c6bdb3b0
georgejr <methungeorge333@gmail.com>
2025-02-15 11:29:35 +0100

    unit test

--- tests/testthat/test_read_asd.R
+++ tests/testthat/test_read_asd.R
@@ -1,38 +1,21 @@
 library(testthat)
 library(SummarizedExperiment)
 
-# Define a temporary directory for testing
-setup_test_data <- function() {
-  test_dir <- tempdir()
-  file.create(file.path(test_dir, "test1.asd"))
-  file.create(file.path(test_dir, "test2.asd"))
-  return(test_dir)
-}
+# Define the path to the test data directory
+test_data_dir <- file.path("/Users/methungeorge/Desktop/nearspectRa/tests/asd_data")
 
-test_that("Function returns a SummarizedExperiment object", {
-  test_dir <- setup_test_data()
-  result <- read_summarizedexperiment_asd(test_dir)
-  expect_s4_class(result, "SummarizedExperiment")
-})
+test_that("SummarizedExperiment contains correct colData and rowData", {
+  result <- read_summarizedexperiment_asd(test_data_dir)
 
-test_that("SummarizedExperiment object contains assay data", {
-  test_dir <- setup_test_data()
-  result <- read_summarizedexperiment_asd(test_dir)
-  expect_true("counts" %in% assayNames(result))
-  expect_true(nrow(assay(result)) > 0)
-  expect_true(ncol(assay(result)) > 0)
-})
+  print("colData columns:")
+  print(colnames(colData(result)))
 
-test_that("SummarizedExperiment contains colData and rowData", {
-  test_dir <- setup_test_data()
-  result <- read_summarizedexperiment_asd(test_dir)
+  print("rowData columns:")
+  print(colnames(rowData(result)))
+
   expect_true(nrow(colData(result)) > 0)
   expect_true(nrow(rowData(result)) > 0)
-})
 
-test_that("Function handles a single file input", {
-  test_dir <- setup_test_data()
-  single_file <- file.path(test_dir, "test1.asd")
-  result <- read_summarizedexperiment_asd(single_file)
-  expect_s4_class(result, "SummarizedExperiment")
+  expect_true(any(grepl("sample", colnames(colData(result)))))
+  expect_true(any(grepl("Wavelength", colnames(rowData(result)))))
 })
